8076afb1f2b0af95def06ac4ca71197b5075a562
SatishKumar <81620964+itssatishkumar@users.noreply.github.com>
2026-02-13 00:03:46 +0530

    Update Launcher.py

--- Launcher.py
+++ Launcher.py
@@ -117,7 +117,7 @@ class SplashWindow(QWidget):
         layout.setAlignment(Qt.AlignCenter)
 
         # Header: Yellow strip w/ red text
-        header = QLabel("CAN LOGGER v1.0.28")
+        header = QLabel("CAN LOGGER v1.0.29")
         header.setFixedHeight(64)
         header.setAlignment(Qt.AlignCenter)
         header.setStyleSheet("""
